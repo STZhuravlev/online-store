208aa199a2300f5e7c2776f50717f7ee507ee487
Stanislav Zhuravlev <suntoo@mail.ru>
2022-12-16 19:30:14 +0300

    Создана модель Category

--- product/admin.py
+++ product/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin  # noqa F401
 from django.utils.translation import gettext_lazy as _
-from product.models import Product, Banner
+from mptt.admin import MPTTModelAdmin
+from product.models import Product, Banner, Category
 
 
 class ProductAdmin(admin.ModelAdmin):
@@ -20,5 +21,15 @@ class BannerAdmin(admin.ModelAdmin):
         verbose_name_plural = _('баннеры')
 
 
+class CategoryAdmin(MPTTModelAdmin):
+    mptt_level_indent = 2
+    list_display = ['category', 'active', 'parent']
+
+    class Meta:
+        verbose_name = _('категория')
+        verbose_name_plural = _('категории')
+
+
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Banner, BannerAdmin)
+admin.site.register(Category, CategoryAdmin)
