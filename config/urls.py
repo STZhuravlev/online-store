c2c4a5f7ad6da9337ba5ccd1e0fb59c4c86b163f
sergei_maltsev <maltsevsergey3129@gmail.com>
2022-12-17 10:02:25 +0300

    .

--- config/urls.py
+++ config/urls.py
@@ -19,5 +19,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/', include('users.urls')),
-    path('change/', include('django.contrib.auth.urls'))
+    path('change/', include('django.contrib.auth.urls')),
+    path('product/', include('product.urls'))
+
 ]
