45408bbeee79f7a054c48b549b7f419da30cf7cf
Your Name <you@example.com>
2023-01-08 11:08:29 +0300

    .

--- config/settings/base.py
+++ config/settings/base.py
@@ -142,7 +142,7 @@ USE_TZ = True
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # пустая папка, сюда будет собирать статику collectstatic
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # пустая папка, сюда будет собирать статику collectstatic
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
