5eae731536e9e3b935aef7974a95e6339f23d0c6
ruslan <danil.2001.danil.2002@gmail.com>
2023-01-25 21:31:30 +0300

    order

--- orders/admin.py
+++ orders/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
-from .models import Order, OrderItem
+from .models import Order
 
 
-class OrderItemInline(admin.TabularInline):
-    model = OrderItem
-    raw_id_fields = ['product']
+# class OrderItemInline(admin.TabularInline):
+#     model = OrderItem
+#     raw_id_fields = ['product']
 
 
 class OrderAdmin(admin.ModelAdmin):
@@ -12,7 +12,7 @@ class OrderAdmin(admin.ModelAdmin):
                     'address', 'postal_code', 'city', 'paid',
                     'created', 'updated']
     list_filter = ['paid', 'created', 'updated']
-    inlines = [OrderItemInline]
+    # inlines = [OrderItemInline]
 
 
 admin.site.register(Order, OrderAdmin)
