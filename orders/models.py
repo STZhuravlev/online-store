ff326eb4e6cebae9908e074fca15015573ea954f
ruslan <danil.2001.danil.2002@gmail.com>
2023-01-27 12:59:52 +0300

    order

--- orders/models.py
+++ orders/models.py
@@ -21,7 +21,7 @@ class Order(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     email = models.EmailField()
-    offer1 = models.ManyToManyField("product.Offer", through="OrderItem", verbose_name=_("продукт"))
+    offer = models.ManyToManyField("shop.Seller", through="OrderItem", verbose_name=_("продукт"))
     address = models.CharField(max_length=250)
     postal_code = models.CharField(max_length=20)
     city = models.CharField(max_length=100)
@@ -46,7 +46,7 @@ class Order(models.Model):
 
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, related_name='items', on_delete=models.PROTECT)
-    offer1 = models.ForeignKey('product.Offer', related_name='order_items', on_delete=models.PROTECT)
+    offer = models.ForeignKey('shop.Seller', related_name='order_items', on_delete=models.PROTECT)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     quantity = models.PositiveIntegerField(default=1)
 
