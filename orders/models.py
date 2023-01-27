e1c9ca40783648aa7a628363c96b2ed05d7a5672
ruslan <danil.2001.danil.2002@gmail.com>
2023-01-27 12:43:04 +0300

    order

--- orders/models.py
+++ orders/models.py
@@ -45,8 +45,8 @@ class Order(models.Model):
 
 
 class OrderItem(models.Model):
-    order = models.ForeignKey(Order, related_name='items', on_delete=models.PROTECT)
     offer = models.ForeignKey('product.Offer', related_name='order_items', on_delete=models.PROTECT)
+    order = models.ForeignKey(Order, related_name='items', on_delete=models.PROTECT)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     quantity = models.PositiveIntegerField(default=1)
 
