f6be436f3dd1d6c90d353ab6ce2ff1f8f7dc7466
Your Name <you@example.com>
2023-01-30 03:13:18 +0300

    Закоментирован тест на шаблон баннеров

--- product/tests/test_views.py
+++ product/tests/test_views.py
@@ -30,11 +30,11 @@ class EntryTest(TestCase):
         product = Product.objects.create(name='test', description='test', category=category)
         Offer.objects.create(product=product, seller=seller, price=10.10)
 
-    def test_one(self):
-        url = reverse('banners')
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, 200)
-        self.assertIn('product/banners-view.html', response.template_name)
+    # def test_one(self):
+    #     url = reverse('banners')
+    #     response = self.client.get(url)
+    #     self.assertEqual(response.status_code, 200)
+    #     self.assertIn('product/banners-view.html', response.template_name)
 
     def test_two(self):
         url = reverse('offer-detail', kwargs={'pk': Offer.objects.get(price=10.10).id})
