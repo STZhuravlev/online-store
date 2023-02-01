f414d19e9469a1ad67ce55db2af0ca181a8dd2da
Your Name <you@example.com>
2023-02-01 20:58:20 +0300

    .

--- product/urls.py
+++ product/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('<int:pk>/', views.ProductDetailView.as_view(), name='product-detail'),
     path('category/', views.CategoryView.as_view(), name='category'),
     path('offer/<int:pk>/', views.OfferDetailView.as_view(), name='offer-detail'),
-    path('catalog', views.CategoryView.as_view(), name='catalog-view'),
+    path('catalog', views.CatalogListView.as_view(), name='catalog-view'),
     path('index', views.IndexView.as_view(), name='index'),
 ]
