dea20b86771e61138262f678b005d0e0628f1530
Sergei Fedorov <s.fedorov.mhi@yandex.ru>
2023-02-01 17:00:22 +0000

    исправление отображения списка категорий в базовом шаблоне

--- product/urls.py
+++ product/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('category/', views.CategoryView.as_view(), name='category'),
     path('catalog/', views.CatalogListView.as_view(), name='catalog-view'),
     path('offer/<int:pk>/', views.OfferDetailView.as_view(), name='offer-detail'),
-    path('catalog', views.CategoryView.as_view(), name='catalog-view'),
-    path('index', views.IndexView.as_view(), name='index'),
+    path('catalog/', views.CategoryView.as_view(), name='catalog-view'),
+    path('index/', views.IndexView.as_view(), name='index'),
 ]
