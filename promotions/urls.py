e6c0bd74523f68fa317cee5a161a9abfbcdd7e42
Sergei Fedorov <s.fedorov.mhi@yandex.ru>
2023-02-06 21:29:06 +0000

    добавляет отображение детальной информации об акции

--- promotions/urls.py
+++ promotions/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from promotions.views import PromoListView
+from promotions.views import PromoListView, PromoDetailView
 
 
 urlpatterns = [
     path('promo/', PromoListView.as_view(), name='promo-list'),
-
+    path('promo/<int:pk>/', PromoDetailView.as_view(), name='promo-detail'),
 ]
--- promotions/views.py
+++ promotions/views.py
@@ -1,9 +1,12 @@
-from django.views.generic import ListView
+from django.core.paginator import Paginator
+from django.views.generic import ListView, DetailView
 from promotions.models import Promo
+from product.models import Product
 from product.services import get_category
 
 
 PROMO_PER_PAGE = 4
+PROMO_PRODUCTS_PER_PAGE = 4
 
 
 class PromoListView(ListView):
@@ -15,4 +18,29 @@ class PromoListView(ListView):
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
         context['categories'] = get_category()
+        return context
+
+
+class PromoDetailView(DetailView):
+    template_name = 'promotions/promo-detail.html'
+    model = Promo
+    context_object_name = 'promo'
+
+    def get_related_products(self):
+        product_list = self.object.promo2products.first()
+        if hasattr(product_list, 'product'):
+            product_list = product_list.product.all()
+        else:
+            product_list = Product.objects.all()
+        paginator = Paginator(product_list, PROMO_PRODUCTS_PER_PAGE)
+        page_number = self.request.GET.get('page')
+        products = paginator.get_page(page_number)
+        return products
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = get_category()
+        products = self.get_related_products()
+        context['products'] = products
+        context['page_obj'] = products
         return context
