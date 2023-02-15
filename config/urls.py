04d6885538d61c44e45b9daebd3b1c762f7fa0ef
Your Name <you@example.com>
2023-02-16 00:56:56 +0300

    Изменения адреса главной страницы + небольшая косметика в базовом шаблоне добавлены ссылки на кнопки

--- config/urls.py
+++ config/urls.py
@@ -17,6 +17,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from product.views import IndexView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,5 +28,6 @@ urlpatterns = [
     path('cart/', include('cart.urls')),
     path('orders/', include('orders.urls')),
     path('promos/', include('promotions.urls', namespace='promo')),
+    path('', IndexView.as_view(), name='index')
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- product/urls.py
+++ product/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path('offer/<int:pk>/', FeedbackDetailView.as_view(), name='offer-detail'),
     path('catalog/', ProductCatalogView.as_view(), name='catalog-view'),
     path('history_view/', HistoryViewsView.as_view(), name='history_view'),
-    path('index/', IndexView.as_view(), name='index'),
+    # path('index/', IndexView.as_view(), name='index'),
 ]
