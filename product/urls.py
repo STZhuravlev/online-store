a4e91c05d76256a2b0fa40bb49800ce126d2bbb3
Your Name <you@example.com>
2023-03-04 21:26:16 +0300

    Переработка механизма сравнения добавление кнопок сравнения на страницы. Удаление старого представления главной страницы. Частичный перенос неиспользуемых шаблонов

--- product/urls.py
+++ product/urls.py
@@ -6,7 +6,6 @@ from product.views import (
     FeedbackDetailView,
     HistoryViewsView,
     ProductCatalogView,
-    IndexView,
     UploadProductFileView
 )
 
@@ -19,6 +18,5 @@ urlpatterns = [
     path('offer/<int:pk>/', FeedbackDetailView.as_view(), name='offer-detail'),
     path('catalog/', ProductCatalogView.as_view(), name='catalog-view'),
     path('history_view/', HistoryViewsView.as_view(), name='history_view'),
-    path('index/', IndexView.as_view(), name='index'),
     path('upload_file/', UploadProductFileView.as_view(), name='upload_file'),
 ]
--- product/views.py
+++ product/views.py
@@ -28,7 +28,6 @@ from product.services import (
     apply_filter_to_catalog,
     apply_sorting_to_catalog,
     get_banners,
-    BannersView,
     ImageView,
     get_favorite_categories,
     get_popular_products,
@@ -96,20 +95,21 @@ class CategoryView(generic.ListView):
         return context
 
 
-class FeedbackDetailView(generic.CreateView):
+class FeedbackDetailView(generic.DetailView, generic.CreateView):
 
     """Детальное отображение продукта, отзывов и добавления отзыва"""
 
-    model = Feedback
+    model = Offer
     form_class = FeedbackForm
     template_name = 'product/offer-detail.html'
+    context_object_name = 'offer'
 
     def get_success_url(self):
         return reverse('offer-detail', kwargs={'pk': self.object.product.pk})
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['offer'] = Offer.objects.filter(product=Offer.objects.get(id=self.kwargs['pk']).product)
+        context['offers'] = Offer.objects.filter(product=Offer.objects.get(id=self.kwargs['pk']).product)
         context['categories'] = get_category()
         context['product_image'] = ProductImage.objects.filter(product=Offer.objects.get(id=self.kwargs['pk']).product)
         context['feedback'] = Feedback.objects.filter(product=Offer.objects.get(id=self.kwargs['pk']).product)
@@ -215,14 +215,14 @@ class ProductCatalogView(generic.ListView):
         return cached_data
 
 
-class IndexView(generic.TemplateView):
-    template_name = 'product/index.html'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['banners'] = BannersView.get_banners()
-        context['categories'] = get_category()
-        return context
+# class IndexView(generic.TemplateView):
+#     template_name = 'product/index.html'
+#
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context['banners'] = BannersView.get_banners()
+#         context['categories'] = get_category()
+#         return context
 
 
 class UploadProductFileView(generic.FormView):
