fc81eb0404ae2f509f5f83859549549c2621dba6
Your Name <you@example.com>
2023-03-10 21:07:36 +0300

    Добавлен context_processor для отображения кол-во товаров в корзине и их стоимости. Настройка сумму доставки в административном разделе сайта.

--- cart/contex_processor.py
+++ cart/contex_processor.py
@@ -2,4 +2,6 @@ from .service import Cart
 
 
 def cart(request):
+    # cart = Cart(request)
+    # quantity = cart.get_total_quantity
     return {'cart': Cart(request)}
--- cart/service.py
+++ cart/service.py
@@ -75,6 +75,9 @@ class Cart:
         del self.session[settings.CART_SESSION_ID]
         self.session.modified = True
 
+    def get_total_quantity(self):
+      return sum(item['quantity'] for item in self.cart.values())
+
     def add_quantity(self, offer):
         offer_id = str(offer.id)
         quantity = 1
