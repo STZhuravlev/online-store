d83e40ba2ed882c5d2a765ecadf25f69f76eb15f
ruslan <danil.2001.danil.2002@gmail.com>
2023-03-15 16:43:17 +0300

    Доработаны страницы продавца и товара, дополнена старница оформления заказа, а так же истории заказов. Добавлена полная локализация на английский язык

--- config/settings/base.py
+++ config/settings/base.py
@@ -188,21 +188,21 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 #         'LOCATION': os.path.join(BASE_DIR, 'cache_files'),
 #     }
 # }
-# CACHES = {
-#     'default': {
-#         'BACKEND': 'django.core.cache.backends.redis.RedisCache',
-#         'LOCATION': 'redis://127.0.0.1:6379',
-#     }
-# }
 CACHES = {
-    "default": {
-        "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379",
-        "OPTIONS": {
-            "CLIENT_CLASS": "django_redis.client.DefaultClient",
-        }
+    'default': {
+        'BACKEND': 'django.core.cache.backends.redis.RedisCache',
+        'LOCATION': 'redis://127.0.0.1:6379',
     }
 }
+# CACHES = {
+#     "default": {
+#         "BACKEND": "django_redis.cache.RedisCache",
+#         "LOCATION": "redis://127.0.0.1:6379/1",
+#         "OPTIONS": {
+#             "CLIENT_CLASS": "django_redis.client.DefaultClient",
+#         }
+#     }
+# }
 
 CELERY_BROKER_URL = os.environ.get('REDIS_URL', 'redis://127.0.0.1:6379/0')
 CELERY_TASK_TRACK_STARTED = True
